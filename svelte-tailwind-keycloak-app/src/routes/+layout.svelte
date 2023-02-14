95f76f920dcba7519c34e4c5072ab6efc733d8e4
Enrico Vadilonga <e.vadilonga@stamtech.com>
2023-02-14 09:44:46 +0100

    add loading condition and authentication failed condition

--- svelte-tailwind-keycloak-app/src/routes/+layout.svelte
+++ svelte-tailwind-keycloak-app/src/routes/+layout.svelte
@@ -1,36 +1,43 @@
-<script lang='ts'>
-  import "../app.css";
-  import { onMount } from 'svelte';
-  import Keycloak from 'keycloak-js';
+<script lang="ts">
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import Keycloak from 'keycloak-js';
 
-    let keycloak: Keycloak;
-    export let isAuth = false;
-    
-    onMount(async () => {
-      keycloak = new Keycloak({
-        realm: 'keycloak',
-        url: 'http://localhost:8080',
-        clientId: 'svelte-app'
-      });
-  
-      try {
-        await keycloak.init({
-          onLoad: 'login-required'
-        });
-        isAuth=true;
-        console.log('User is authenticated');
-      } catch (error) {
-        console.error(error);
-      }
-    });
+	let keycloak: Keycloak;
+	let isAuth: boolean = false;
+	let isLoading: boolean = true;
 
+	onMount(async () => {
+		keycloak = new Keycloak({
+			realm: 'keycloak',
+			url: 'http://localhost:8080',
+			clientId: 'svelte-app'
+		});
+
+		try {
+			await keycloak.init({
+				onLoad: 'login-required'
+			});
+			isAuth = true;
+			console.log('User is authenticated');
+		} catch (error) {
+			isAuth = false;
+			console.error(error);
+		}
+		isLoading = false;
+	});
 </script>
-{#if isAuth}
-<main>
-  <slot/>
-</main>
+
+{#if isLoading}
+	<div>loading</div>
 {:else}
-Authentication failed
+	<div>
+		{#if isAuth}
+			<main>
+				<slot />
+			</main>
+		{:else}
+			<div>Authentication failed</div>
+		{/if}
+	</div>
 {/if}
-
-
